baa8e90cd1d00ada6fc6f1cc16651dffe012d3b6
Bobby Bilo <bobbybilo@icloud.com>
2025-05-24 16:14:49 -0400

    initial commit

---
+++ KickSim/KickSim/KickSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickSimApp.swift
+//  KickSim
+//
+//  Created by Bobby Bilo on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct KickSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KickSim/KickSimTests/KickSimTests.swift
@@ -0,0 +1,16 @@
+//
+//  KickSimTests.swift
+//  KickSimTests
+//
+//  Created by Bobby Bilo on 5/24/25.
+//
+
+import Testing
+
+struct KickSimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
